73903e91340013b3d801319c6e2fd87c2adf97e1
Azim <azim@vikass-mac-mini.local>
2017-03-18 19:08:08 +0530

    Initial Commit

---
+++ Azim Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Azim Project
+//
+//  Created by Azim on 18/03/17.
+//  Copyright © 2017 mobitrail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
